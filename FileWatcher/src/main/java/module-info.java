afd9b22fa2f796ef82db7a5e6a227ff698a3e688
Kjw072892 <kassie.j.whitney92@gmail.com>
2025-07-16 18:50:48 -0700

    Created FileWatcher Skeleton

---
+++ FileWatcher/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.tcss_360.filewatcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.tcss_360.filewatcher to javafx.fxml;
+    exports com.tcss_360.filewatcher;
+}
